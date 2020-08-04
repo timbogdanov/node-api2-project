d5aad56a82744d8ce69a9864b29a5d9035064901
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-04 13:55:19 -0700

    handle fetching specified post with id

--- routers/posts/posts-router.js
+++ routers/posts/posts-router.js
@@ -66,4 +66,23 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  try {
+    Posts.findById(id)
+      .then((post) => {
+        res.status(200).json(post);
+      })
+      .catch((error) => {
+        res.status(404).json({
+          message: 'The post with the specified ID does not exist.',
+        });
+      });
+  } catch (error) {
+    res.status(500).json({
+      error: 'The post information could not be retrieved.',
+    });
+  }
+});
+
 module.exports = router;
