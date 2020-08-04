ec9603f7f99396ddc5dfc7e108e4d9274a8cccb1
Timofey Bogdanov <timofeybogdanov@Timofeys-iMac.local>
2020-08-04 13:43:24 -0700

    handle retreving all posts

--- routers/posts/posts-router.js
+++ routers/posts/posts-router.js
@@ -54,4 +54,16 @@ router.post('/:id/comments', (req, res) => {
   }
 });
 
+router.get('/', (req, res) => {
+  Posts.find()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error: 'The posts information could not be retrieved.',
+      });
+    });
+});
+
 module.exports = router;
